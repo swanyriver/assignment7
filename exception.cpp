0b034b6cfe02497e5c42131e042d18cc08d569fd
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2014-08-07 16:58:32 -0700

    exception written, auto tested with text files

--- exception.cpp
+++ exception.cpp
@@ -17,43 +17,62 @@
  * *
  * ***********************************************************/
 
+
+/*
+ * Tested with following bash command ./bin 2>log.txt 0<inputTest.txt
+ */
+
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include "SwansonLibs/swansonInput.hpp"
 
 using namespace std;
 
-void demonstrateComponent();
+class ItemNotFound{};
+ItemNotFound myNoItem;
 
+
+int getProductID ( int ids[] , string names[] , int numProducts ,
+      string target ) throw (ItemNotFound) {
+   for ( int i = 0 ; i < numProducts ; i++ ) {
+      if ( names[i] == target )
+         return ids[i];
+   }
+   throw myNoItem; // Not found
+}
 
 /////////////////////////////////////////////////
 ////////////Introduction and repetition ////////
 ////////////////////////////////////////////////
-int main(){
+int main () {
 
-   string intro = "";
+   int productIds[] = {4, 5, 8, 10, 13};
+   string products[] = {"computer","flash drive",
+                           "mouse","printer","camera"};
 
-   do{
 
-     cout << intro;
-     demonstrateComponent();
+   string prompt = "Enter a product name and I can look up its ID number or"
+         " \"EXIT\" to quit\nEnter product name now:";
 
-   } while ( swansonInput::yesNo( "Would you like to see that again" ) );
+
+
+   while(true) {
+
+      string productName = swansonInput::GetString(prompt);
+      if(productName=="EXIT") exit(1);
+
+      try{
+         int productId = getProductID(productIds,products,5,productName);
+         cerr << "Product ID for " << productName
+               << " is:" << productId << endl;
+      }catch (ItemNotFound){
+         cerr << productName << " was not found" << endl;
+      }
+
+   }
 
    return 0;
 
 }
-
-
-/////////////////////////////////////////////////
-////////////Exercise Component /////////////////
-////////////////////////////////////////////////
-
-void demonstrateComponent(){
-
-
-}
-
-
-
 
